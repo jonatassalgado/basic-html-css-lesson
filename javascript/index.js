9b334931fd6ced3ad36ff7c03ccee1049524c16b
jonatassalgado <jonataseduardosalgado@gmail.com>
2022-03-30 17:30:36 -0300

    Add challenge to README

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,30 @@
-document.addEventListener('DOMContentLoaded', event => {
+document.addEventListener('DOMContentLoaded', function (event) {
     console.log('Agora posso ver o HTML')
 
     const requestFriendshipEl = document.querySelector("#request-friendship")
 
-    requestFriendshipEl.addEventListener('click', event => {
+    requestFriendshipEl.addEventListener('click', function (event) {
         const clickedElement = event.target
+        const isFriendshipRequested = clickedElement.dataset.requestedFriendship === 'true'
 
-        if(clickedElement.dataset.requestedFriendship === 'true') {
+        if (isFriendshipRequested) {
             clickedElement.innerText = "Solicitar amizade"
-
             clickedElement.classList.remove('button--active')
-            /* clickedElement.style.backgroundColor = "#3f51b5"
-            clickedElement.style.borderColor = "#29377e" */
-
             clickedElement.dataset.requestedFriendship = false
         } else {
             clickedElement.innerText = "Amizade solicitada"
-
             clickedElement.classList.add('button--active')
-            /* clickedElement.style.backgroundColor = "green"
-            clickedElement.style.borderColor = "darkGreen" */
-
             clickedElement.dataset.requestedFriendship = true
         }
     })
+
+
+    /* Abaixo você vai ouvir quando o botão de enviar mensagem for clicado 
+    e vai mostrar o elemtno com id "message" mudadndo o atributo "display" 
+    para "inline-block" */
+
+
+
+    
 
 })
